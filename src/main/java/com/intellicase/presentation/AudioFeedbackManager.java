3572a94c5a9267f9594ba94299b656bc6c11e344
T361 <tshaukat.25@gmail.com>
2026-04-28 02:05:56 +0500

    Fix: Remove unused import to resolve Checkstyle error

--- src/main/java/com/intellicase/presentation/AudioFeedbackManager.java
+++ src/main/java/com/intellicase/presentation/AudioFeedbackManager.java
@@ -7,7 +7,6 @@ import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
 
 import javafx.scene.Node;
 
